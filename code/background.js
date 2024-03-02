c8ce6f4833954ff0ec361b63ae6bb5c0d5a17f68
030srinivas <030shrinivasr@gmail.com>
2024-03-02 02:15:22 -0500

    commit

--- code/background.js
+++ code/background.js
@@ -1,4 +1,3 @@
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "scrapeWebsite") {
     // Handle the scrapeWebsite action
@@ -19,13 +18,30 @@ chrome.runtime.onInstalled.addListener(function () {
     id: "searchGoogle"
   });
 
-  chrome.contextMenus.create({
-    title: "Pronunciation",
+  // chrome.contextMenus.create({
+  //   title: "Pronunciation",
+  //   contexts: ["selection"],
+  //   id: "pronounceWord"
+  // });
+// new add by shreesha from here -1
+   chrome.contextMenus.create({
+    title: "Add to review",
     contexts: ["selection"],
-    id: "pronounceWord"
+    id: "addToReview"
   });
+//till here-1
+
 });
 
+function storeSelectedText(selectedText) {
+  chrome.storage.local.get('reviewList', function(data) {
+    const reviewList = data.reviewList || [];
+    reviewList.push(selectedText);
+    chrome.storage.local.set({ 'reviewList': reviewList }, function() {
+      console.log('Selected text stored in local storage:', selectedText);
+    });
+  });
+}
 // Handle context menu click
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
   var selectedText = info.selectionText.trim();
@@ -35,14 +51,19 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
     searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(selectedText + " pronunciation");
   }
 
-  if (info.menuItemId === "pronounceWord") {
-    // Send a message to content script to handle pronunciation
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "pronounceWord", selectedText: selectedText });
-    });
-    return;
+  
+  // if (info.menuItemId === "pronounceWord") {
+  //   // Send a message to content script to handle pronunciation
+  //   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  //     chrome.tabs.sendMessage(tabs[0].id, { action: "pronounceWord", selectedText: selectedText });
+  //   });
+  //   return;
+  // }
+  // by shreesha from here -2
+  if (info.menuItemId === 'addToReview' && info.selectionText) {
+    storeSelectedText(info.selectionText);
   }
-
+  // till here -2
   if (searchUrl) {
     chrome.tabs.create({ url: searchUrl }, function (createdTab) {
       setTimeout(function () {
